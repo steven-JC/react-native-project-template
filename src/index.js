f701c1d76ca57f23830824ca552c1bbe56594fa5
steven03.chen <steven03.chen@vipshop.com>
2016-10-14 20:25:25 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
  * Created by steven on 10/13/16.
  */
 import React, { Component } from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
+import Orientation from 'react-native-orientation';
 
 import tunk from "tunk";
 import actionMiddleware from "tunk-action-middleware";
@@ -16,9 +17,17 @@ import NavBar from './components/NavBar';
 tunk.addMiddleware([actionMiddleware]);
 
 export default class App extends Component {
+
+    componentWillMount(){
+        if(Platform.OS == 'ios')
+            Orientation.lockToLandscape();
+    }
+
     render() {
         return (<Navigator scenes={scenes} NavBar={NavBar} />);
     }
 }
+
+
 
 AppRegistry.registerComponent('v35', () => App);
